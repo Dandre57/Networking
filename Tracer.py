35cc372b837737094e1d033ad78210c2a7ab8bf0
D'Andre Campbell <dandrecampbell5@gmail.com>
2016-11-30 18:44:00 -0500

    Create Tracer.py

---
+++ Tracer.py
@@ -0,0 +1,14 @@
+from tld import get_tld
+import os
+
+def get_domain_name(url):
+    domain_name = get_tld(url)
+    return domain_name
+
+"""
+Method that runs tracert command
+"""
+
+"""
+Main method
+"""
