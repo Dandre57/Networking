dd57b493e05baea8e88ac16521790aa5fbf82e49
D'Andre Campbell <dandrecampbell5@gmail.com>
2016-12-02 16:53:48 -0500

    Update Client.py

--- Client.py
+++ Client.py
@@ -1,1 +1,19 @@
 import socket as sock
+
+host = sock.gethostname()
+
+try:
+  sock.bind()
+  sock.listen()
+  
+  sock.connect(host, 1234)
+  print("Connecting to socket...\n")
+  
+  sock.recv(100)
+  print("Receiving...\n")
+  
+  sock.close()
+  print("Closing the socket...\n")
+  
+except Exception:
+  print("Could not connect.")
